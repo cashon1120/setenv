7f8bc73c4000ec7ee22c6115124c8b1f4617c413
cashon1120 <49798613@qq.com>
2023-06-14 10:27:18 +0800

    目录修改

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = 'http://dev.webapi.emhes.cn:13201';
+export const BASE_URL = 'http://192.168.10.9:13201';
 export const SOCKET_RUL = 'wss://ws.emhes.cn/ws';
 export const WEBSOCKET_API = 'https://websocketapi.emhes.cn';
