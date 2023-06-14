2eb86559fcb2414e8717089f42fdc4f3d4bf05ae
cashon1120 <49798613@qq.com>
2023-06-14 10:18:56 +0800

    first

---
+++ bin/util.js
@@ -0,0 +1,14 @@
+const getParams = file => {
+  const keys = file.split('\n');
+  let fileContent = '';
+  keys.forEach((str, index) => {
+    keys[index] = str.replace(/\s/g, '');
+    if (keys[index].indexOf('=') > 0) {
+      const [key, value] = keys[index].split('=');
+      fileContent = fileContent + `export const ${key} = '${value}';\n`;
+    }
+  });
+  return fileContent;
+};
+
+export default getParams
---
+++ src/config/index.ts
@@ -0,0 +1,3 @@
+export const BASE_URL = 'http://dev.webapi.emhes.cn:13201';
+export const SOCKET_RUL = 'wss://ws.emhes.cn/ws';
+export const WEBSOCKET_API = 'https://websocketapi.emhes.cn';
